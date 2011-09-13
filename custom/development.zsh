3dc0bfacda2270919aa3a6196a3b23ee4bea6822
Jason Harrelson <jason@lookforwardenterprises.com>
2011-09-12 23:36:33 -0500

    Add some aliases for git bisect and ps grep for searchd.

--- custom/development.zsh
+++ custom/development.zsh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 
 alias fsc-gvt='/usr/local/bin/searchd --stop --config "/Users/cjharrelson/development/iberon/gvt-us/config/development.sphinx.conf"'
+alias ps-searchd='ps -ea | grep "searchd"'
