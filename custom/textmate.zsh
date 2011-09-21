5ee8b60a15f54360a92f286fd2b6d7ec1cddb2d6
Jeff Junatas <jjunatas@gmail.com>
2011-09-21 17:43:18 -0500

    Updating to remove some Jason specific items.

--- custom/textmate.zsh
+++ custom/textmate.zsh
@@ -2,5 +2,5 @@
 
 alias m='mate .'
 
-alias mate-oh_my_zsh='mate /Users/cjharrelson/.oh-my-zsh'
-alias mate-dotfiles='mate /Users/cjharrelson/.dotfiles'
+alias mate-oh_my_zsh='mate /Users/jjunatas/.oh-my-zsh'
+alias mate-dotfiles='mate /Users/jjunatas/.dotfiles'
